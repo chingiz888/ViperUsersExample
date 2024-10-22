6b5023d9f22a612afa7c9245d2f8d21f8a69ac3e
Chingiz iOS Developer <c.bayshurin@kaller.io>
2024-10-22 22:26:53 +0300

    Initial Commit

---
+++ ViperUsersExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViperUsersExample
+//
+//  Created by mac_pro14 on 22.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
